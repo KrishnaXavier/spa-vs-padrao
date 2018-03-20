06adaa243899d2257f9791e31adcb37de58912da
KrishnaXavier <tsixav@gmail.com>
2018-03-20 02:14:38 -0300

    spa pronto

--- spa/home.php
+++ spa/home.php
@@ -1,3 +1,72 @@
 <div id="saida-dados">
-	<h1>Home</h1>
+	
+
+	<div class="row">
+		<div class="col s10 offset-s1">
+			<h1>Home</h1>
+
+			<table>
+				<thead>
+					<tr>
+						
+						<th>Pagina</th>
+
+						<th>SPA - Arquivos</th>
+						<th>SPA - Tamanho</th>
+
+						<th>padrão - Arquivos</th>
+						<th>padrão - Tamanho</th>
+
+					</tr>
+				</thead>
+
+				<tbody>
+					
+					<tr>
+						<td>index</td>
+
+						<td>7</td>
+						<td>401KB</td>
+
+						<td>7</td>
+						<td>401KB</td>
+					</tr>
+					
+					<tr>
+						<td>home</td>
+
+						<td>1</td>
+						<td>2KB</td>
+
+						<td>7</td>
+						<td>401KB</td>
+					</tr>
+
+					<tr>
+						<td>noticias</td>
+
+						<td>4</td>
+						<td>370KB</td>
+
+						<td>10</td>
+						<td>701KB</td>
+					</tr>
+
+					<tr>
+						<td>cadastro</td>
+
+						<td>1</td>
+						<td>1.8KB</td>
+
+						<td>7</td>
+						<td>403KB</td>
+					</tr>
+
+				</tbody>
+			</table>
+
+		</div>
+
+	</div>
+
 </div>
