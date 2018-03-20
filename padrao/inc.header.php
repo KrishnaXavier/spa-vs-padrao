a39b8bbf48dab4a149a511a93e6fc329e046cd43
KrishnaXavier <tsixav@gmail.com>
2018-03-20 00:08:22 -0300

    Estrutura da padrão

---
+++ padrao/inc.header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>	
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">	
+	<link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>	
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	<link type="text/css" rel="stylesheet" href="css/style.css"/>		
+	<link rel="shortcut icon" href="https://s.4cdn.org/image/favicon.ico">	
+	<title>Aplicação</title>	
+</head>
+
+<body>	
+	<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+	<script type="text/javascript" src="js/materialize.min.js"></script>
+
+	<nav>
+		<div class="nav-wrapper">
+			<a href="index.php" class="brand-logo">HOME</a>
+			<ul id="nav-mobile" class="right hide-on-med-and-down">
+				<li><a href="noticias.php">Notícias</a></li>
+				<li><a href="entrar.php">Entrar</a></li>				
+			</ul>
+		</div>
+	</nav>
