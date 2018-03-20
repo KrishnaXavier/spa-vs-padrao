1664154a6dfbfd8e300e5ff6fbe0cd50218d639b
KrishnaXavier <tsixav@gmail.com>
2018-03-20 02:11:10 -0300

    padrao completo, fazendo tabela

--- padrao/home.php
+++ padrao/home.php
@@ -3,5 +3,74 @@ require_once "inc.header.php";
 ?>
 
 <div id="saida-dados">
-	<h1>Home</h1>
+	
+
+	<div class="row">
+		<div class="col s10 offset-s1">
+			<h1>Home</h1>
+
+			<table>
+				<thead>
+					<tr>
+						
+						<th>Pagina</th>
+
+						<th>SPA - Arquivos</th>
+						<th>SPA - Tamanho</th>
+
+						<th>padrão - Arquivos</th>
+						<th>padrão - Tamanho</th>
+
+					</tr>
+				</thead>
+
+				<tbody>
+					
+					<tr>
+						<td>index</td>
+
+						<td>7</td>
+						<td>401KB</td>
+
+						<td>7</td>
+						<td>401KB</td>
+					</tr>
+					
+					<tr>
+						<td>home</td>
+
+						<td>1</td>
+						<td>2KB</td>
+
+						<td>7</td>
+						<td>401KB</td>
+					</tr>
+
+					<tr>
+						<td>noticias</td>
+
+						<td>1</td>
+						<td>2KB</td>
+
+						<td>10</td>
+						<td>701KB</td>
+					</tr>
+
+					<tr>
+						<td>cadastro</td>
+
+						<td>1</td>
+						<td>2KB</td>
+
+						<td>7</td>
+						<td>403KB</td>
+					</tr>
+
+				</tbody>
+			</table>
+
+		</div>
+
+	</div>
+
 </div>
--- padrao/noticias.php
+++ padrao/noticias.php
@@ -3,5 +3,58 @@ require_once "inc.header.php";
 ?>
 
 <div id="saida-dados">
-	<h1>Notícias</h1>
+	
+	<div class="row">
+		<div class="col s10 offset-s1">
+			<h1>Notícias</h1>
+		</div>
+	</div>
+
+	<div class="row">
+		
+		<div class="col s6 m4">
+			<div class="card">
+				<div class="card-image">
+					<img src="sample-1.jpg">
+					<span class="card-title">Noticia 1</span>
+				</div>
+				<div class="card-content">
+					<p>Texto texto texto texto texto texto texto texto texto texto texto texto texto texto texto texto texto.</p>
+				</div>
+				<div class="card-action">
+					<a href="#">link</a>
+				</div>
+			</div>
+		</div>
+
+		<div class="col s6 m4">
+			<div class="card">
+				<div class="card-image">
+					<img src="office.jpg">
+					<span class="card-title">Noticia 2</span>
+				</div>
+				<div class="card-content">
+					<p>Texto texto texto texto texto texto texto texto texto texto texto texto texto texto texto texto texto.</p>
+				</div>
+				<div class="card-action">
+					<a href="#">link</a>
+				</div>
+			</div>
+		</div>
+
+		<div class="col s6 m4">
+			<div class="card">
+				<div class="card-image">
+					<img src="sample-1.jpg">
+					<span class="card-title">Noticia 3</span>
+				</div>
+				<div class="card-content">
+					<p>Texto texto texto texto texto texto texto texto texto texto texto texto texto texto texto texto texto.</p>
+				</div>
+				<div class="card-action">
+					<a href="#">link</a>
+				</div>
+			</div>
+		</div>
+
 </div>
