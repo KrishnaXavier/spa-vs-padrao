1bc66e867b73a117a3c42b85d493dfc9d6416830
KrishnaXavier <tsixav@gmail.com>
2018-04-11 09:44:53 -0300

    refatoração

--- spa/inc.header.php
+++ spa/inc.header.php
@@ -70,17 +70,7 @@
 				});
 			}
 		}
-
-		/*
-		Calculate a 32 bit FNV-1a hash
-		Found here: https://gist.github.com/vaiorabbit/5657561
-		Ref.: http://isthe.com/chongo/tech/comp/fnv/
-				@param {string} str the input value
-		@param {boolean} [asString=false] set to true to return the hash value as 
-		    8-digit hex string instead of an integer
-		@param {integer} [seed] optionally pass the hash of the previous chunk
-		@returns {integer | string}
-		*/
+		
 		function hashFnv32a(str, asString, seed) {
 			/*jshint bitwise:false */
 			let i, l,
