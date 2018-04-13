17a76daaf78c411417097c871a7c9b94ce46d674
KrishnaXavier <tsixav@gmail.com>
2018-04-13 09:22:47 -0300

    spa express

---
+++ spa-express/home.php
@@ -0,0 +1,72 @@
+<div id="saida-dados">
+	
+
+	<div class="row">
+		<div class="col s10 offset-s1">
+			<h1>Home</h1>
+
+			<table>
+				<thead>
+					<tr>
+						
+						<th>Pagina</th>
+
+						<th>SPA - Arquivos</th>
+						<th>SPA - Tamanho</th>
+
+						<th>padrão - Arquivos</th>
+						<th>padrão - Tamanho</th>
+
+					</tr>
+				</thead>
+
+				<tbody>
+					
+					<!-- <tr>
+						<td>index</td>
+
+						<td>7</td>
+						<td>401KB</td>
+
+						<td>7</td>
+						<td>697,442KB</td>
+					</tr> -->
+					
+					<tr>
+						<td>home</td>
+
+						<td>8</td>
+						<td>699,169 KB</td>
+
+						<td>8</td>
+						<td>697,442 KB</td>
+					</tr>
+
+					<tr>
+						<td>noticias</td>
+
+						<td>4</td>
+						<td>396,6 KB</td>
+
+						<td>10</td>
+						<td>1029,069 KB</td>
+					</tr>
+
+					<tr>
+						<td>cadastro</td>
+
+						<td>1</td>
+						<td>1,8 KB</td>
+
+						<td>7</td>
+						<td>634,169 KB</td>
+					</tr>
+
+				</tbody>
+			</table>
+
+		</div>
+
+	</div>
+
+</div>
---
+++ spa-express/noticias.php
@@ -0,0 +1,56 @@
+<div id="saida-dados">
+	
+	<div class="row">
+		<div class="col s10 offset-s1">
+			<h1>Notícias</h1>
+		</div>
+	</div>
+
+	<div class="row">
+		
+		<div class="col s6 m4">
+			<div class="card">
+				<div class="card-image">
+					<img src="sample-1.jpg">
+					<span class="card-title">Noticia 1 - EDIÇÃO 2</span>
+				</div>
+				<div class="card-content">
+					<p>Texto texto texto texto texto texto texto texto texto texto texto texto texto texto texto texto texto.</p>
+				</div>
+				<div class="card-action">
+					<a href="#">link</a>
+				</div>
+			</div>
+		</div>
+
+		<div class="col s6 m4">
+			<div class="card">
+				<div class="card-image">
+					<img src="office.jpg">
+					<span class="card-title">Noticia 2</span>
+				</div>
+				<div class="card-content">
+					<p>Texto texto texto texto texto texto texto texto texto texto texto texto texto texto texto texto texto.</p>
+				</div>
+				<div class="card-action">
+					<a href="#">link</a>
+				</div>
+			</div>
+		</div>
+
+		<div class="col s6 m4">
+			<div class="card">
+				<div class="card-image">
+					<img src="sample-1.jpg">
+					<span class="card-title">Noticia 3</span>
+				</div>
+				<div class="card-content">
+					<p>Texto texto texto texto texto texto texto texto texto texto texto texto texto texto texto texto texto.</p>
+				</div>
+				<div class="card-action">
+					<a href="#">link</a>
+				</div>
+			</div>
+		</div>
+
+</div>
